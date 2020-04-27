f1d17c3ccb4f4e021cc0ce4dcc20101d58b19c08
Mariam Alaeddin <mariamalaeddin@gmail.com>
2020-04-27 11:33:48 -0700

    meaninful fail for adding ticket to ticket list
    Co-authored-by: Jose Amesquita <joseamesquita11@gmail.com>
    Co-authored-by: James Carlos <jonathancarlos21@hotmail.com>

--- src/__tests__/reducers/ticket-list-reducer.test.js
+++ src/__tests__/reducers/ticket-list-reducer.test.js
@@ -1,9 +1,40 @@
 import ticketListReducer from '../../reducers/ticket-list-reducer';
 
+
+
 //variable = ticketListReducer
 //ticketListReducer takes 2 arguments (1: current state, 2: an action being applied to the current state)
 describe('ticketListReducer', () => {
+
+  let action;
+  const tickeData = {
+    names: "Ryan and Aimen",
+    location: "4b",
+    issue: "Redux action is not working correctly, cause I suck at it.",
+    id: 1
+  };
+
   test('Should return default state if there is no action type passed into the reducer', () => {
     expect(ticketListReducer({}, { type: null })).toEqual({});
+  });
+  
+  test('Should successfully add new ticket data to masterTicketList', () => {
+    const {names, location, issue, id} = tickeData;
+    action = {
+      type: 'ADD_TICKET',
+      names: names,
+      location: location,
+      issue: issue,
+      id: id
+    };
+    
+    expect(ticketListReducer({},action)).toEqual({
+      [id] : {
+        names: names,
+        location: location,
+        issue: issue,
+        id: id
+      }
+    });
   });
 });
